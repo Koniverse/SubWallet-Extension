101c9a9c7990d7ef9d413c664236e27a33a58e9f
S2kael <laiducminh1002@gmail.com>
2025-02-25 20:25:31 +0700

    [Issue-3658] Handle fallback gasLimit for transfer native

--- packages/extension-base/src/services/balance-service/transfer/smart-contract.ts
+++ packages/extension-base/src/services/balance-service/transfer/smart-contract.ts
@@ -12,6 +12,7 @@ import { combineEthFee } from '@subwallet/extension-base/utils';
 import BigN from 'bignumber.js';
 import { t } from 'i18next';
 import { TransactionConfig } from 'web3-core';
+import { ContractSendMethod } from 'web3-eth-contract';
 
 interface TransferEvmProps extends TransactionFee {
   chain: string;
@@ -21,10 +22,12 @@ interface TransferEvmProps extends TransactionFee {
   transferAll: boolean;
   value: string;
   evmApi: _EvmApi;
+  fallbackFee?: boolean;
 }
 
 export async function getEVMTransactionObject ({ chain,
   evmApi,
+  fallbackFee,
   feeCustom: _feeCustom,
   feeInfo: _feeInfo,
   feeOption,
@@ -44,7 +47,13 @@ export async function getEVMTransactionObject ({ chain,
     ...feeCombine
   } as TransactionConfig;
 
-  const gasLimit = await evmApi.api.eth.estimateGas(transactionObject);
+  const gasLimit = await evmApi.api.eth.estimateGas(transactionObject).catch((e) => {
+    if (fallbackFee) {
+      return 21000;
+    } else {
+      throw e;
+    }
+  });
 
   transactionObject.gas = gasLimit;
 
@@ -101,7 +110,7 @@ export async function getERC20TransactionObject (
 
   const transferData = generateTransferData(to, transferValue);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-  const gasLimit = await erc20Contract.methods.transfer(to, transferValue).estimateGas({ from }) as number;
+  const gasLimit = await (erc20Contract.methods.transfer(to, transferValue) as ContractSendMethod).estimateGas({ from });
   const feeInfo = _feeInfo as EvmFeeInfo;
   const feeCombine = combineEthFee(feeInfo, feeOption, feeCustom);
 
@@ -131,6 +140,7 @@ interface TransferERC20Props extends TransactionFee {
   to: string;
   transferAll: boolean;
   value: string;
+  fallbackFee?: boolean;
 }
 
 export async function getERC721Transaction (
