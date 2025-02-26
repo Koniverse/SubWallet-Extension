1994dbf393ecbb780c17301da13c3a01314a36f7
S2kael <laiducminh1002@gmail.com>
2025-02-26 16:49:00 +0700

    [Merge] Add api url for prod

--- packages/extension-base/src/constants/environment.ts
+++ packages/extension-base/src/constants/environment.ts
@@ -5,4 +5,4 @@ const PRODUCTION_BRANCHES = ['master', 'webapp', 'webapp-dev'];
 const branchName = process.env.BRANCH_NAME || 'subwallet-dev';
 
 export const isProductionMode = PRODUCTION_BRANCHES.indexOf(branchName) > -1;
-export const BACKEND_API_URL = process.env.SUBWALLET_API || (isProductionMode ? '' : 'https://be-dev.subwallet.app/api'); // TODO: Update production URL
+export const BACKEND_API_URL = process.env.SUBWALLET_API || (isProductionMode ? 'https://sw-services.subwallet.app/api' : 'https://be-dev.subwallet.app/api'); // TODO: Update production URL
