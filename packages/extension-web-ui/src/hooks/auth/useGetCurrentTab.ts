c554171b8e328ae74e8868351883c4d44da35468
Frenkie Nguyen <nguyentiendung.me@gmail.com>
2025-05-14 17:17:50 +0700

    [Issue-3784] refactor: refactor: remove unnecessary code

--- packages/extension-web-ui/src/hooks/auth/useGetCurrentTab.ts
+++ packages/extension-web-ui/src/hooks/auth/useGetCurrentTab.ts
@@ -7,30 +7,9 @@ export const useGetCurrentTab = () => {
   const [tab, setTab] = useState<chrome.tabs.Tab | undefined>(undefined);
 
   useEffect(() => {
-    const updateTab = () => {
-      chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-        setTab(tabs[0]);
-      });
-    };
-
-    updateTab();
-
-    chrome.tabs.onActivated.addListener(updateTab);
-    chrome.windows.onFocusChanged.addListener(updateTab);
-
-    const onTabUpdated = (tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tabInfo: chrome.tabs.Tab) => {
-      if (changeInfo.status === 'complete' || changeInfo.url) {
-        updateTab();
-      }
-    };
-
-    chrome.tabs.onUpdated.addListener(onTabUpdated);
-
-    return () => {
-      chrome.tabs.onActivated.removeListener(updateTab);
-      chrome.windows.onFocusChanged.removeListener(updateTab);
-      chrome.tabs.onUpdated.removeListener(onTabUpdated);
-    };
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+      setTab(tabs[0]);
+    });
   }, []);
 
   return tab;
