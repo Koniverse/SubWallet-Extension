d35920d7ac1b16c66518728800176a375a114b22
Thiendekaco <139972251+Thiendekaco@users.noreply.github.com>
2025-05-16 18:55:58 +0700

    [Issue-4365] feat: add ecdsa scheme for polkadot migration

--- packages/extension-koni-ui/src/connector/Ledger/SubstrateGenericLedger.ts
+++ packages/extension-koni-ui/src/connector/Ledger/SubstrateGenericLedger.ts
@@ -1,13 +1,15 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { LEDGER_SCHEME } from '@subwallet/extension-base/background/KoniTypes';
 import { wrapBytes } from '@subwallet/extension-dapp';
 import { PolkadotGenericApp } from '@zondax/ledger-substrate';
+import { GenericeResponseAddress } from '@zondax/ledger-substrate/dist/common';
 
 import { LEDGER_SUCCESS_CODE } from '@polkadot/hw-ledger/constants';
 import { AccountOptions, LedgerAddress, LedgerSignature, LedgerVersion } from '@polkadot/hw-ledger/types';
 import { transports } from '@polkadot/hw-ledger-transports';
-import { hexAddPrefix, hexStripPrefix, u8aToHex } from '@polkadot/util';
+import { hexAddPrefix, hexStripPrefix, u8aConcat, u8aToHex } from '@polkadot/util';
 
 import { BaseLedger } from './BaseLedger';
 
@@ -24,6 +26,8 @@ export async function loadWasm () {
 
 export class SubstrateGenericLedger extends BaseLedger<PolkadotGenericApp> {
   protected ss58_addr_type = 42;
+  protected scheme = LEDGER_SCHEME.ED25519;
+
   getVersion (): Promise<LedgerVersion> {
     return this.withApp(async (app): Promise<LedgerVersion> => {
       const { deviceLocked: locked, major, minor, patch, testMode } = await app.getVersion();
@@ -47,11 +51,18 @@ export class SubstrateGenericLedger extends BaseLedger<PolkadotGenericApp> {
   getAddress (confirm?: boolean, accountOffset?: number, addressOffset?: number, accountOptions?: Partial<AccountOptions>): Promise<LedgerAddress> {
     return this.withApp(async (app): Promise<LedgerAddress> => {
       const path = this.serializePath(accountOffset, addressOffset, accountOptions);
-      const { address, pubKey } = await this.wrapError(app.getAddressEd25519(path, this.ss58_addr_type, confirm));
+      let result: GenericeResponseAddress;
+
+      if (this.scheme === LEDGER_SCHEME.ECDSA) {
+        console.log(this.scheme, path);
+        result = await this.wrapError(app.getAddressEcdsa(path, confirm));
+      } else {
+        result = await this.wrapError(app.getAddressEd25519(path, this.ss58_addr_type, confirm));
+      }
 
       return {
-        address,
-        publicKey: hexAddPrefix(pubKey)
+        address: result.address,
+        publicKey: hexAddPrefix(result.pubKey)
       };
     });
   }
@@ -59,11 +70,20 @@ export class SubstrateGenericLedger extends BaseLedger<PolkadotGenericApp> {
   async signTransaction (message: Uint8Array, metadata: Uint8Array, accountOffset?: number, addressOffset?: number, accountOptions?: Partial<AccountOptions>): Promise<LedgerSignature> {
     return this.withApp(async (app): Promise<LedgerSignature> => {
       const path = this.serializePath(accountOffset, addressOffset, accountOptions);
-      const rs = await this.wrapError((app.signWithMetadataEd25519(path, Buffer.from(message), Buffer.from(metadata))));
 
-      return {
-        signature: hexAddPrefix(u8aToHex(rs.signature))
-      };
+      if (this.scheme === LEDGER_SCHEME.ECDSA) {
+        const { r, s, v } = await this.wrapError(app.signWithMetadataEcdsa(path, Buffer.from(message), Buffer.from(metadata)));
+
+        return {
+          signature: hexAddPrefix(u8aToHex(u8aConcat(r, s, v)))
+        };
+      } else {
+        const rs = await this.wrapError((app.signWithMetadataEd25519(path, Buffer.from(message), Buffer.from(metadata))));
+
+        return {
+          signature: hexAddPrefix(u8aToHex(rs.signature))
+        };
+      }
     });
   }
 
@@ -71,18 +91,26 @@ export class SubstrateGenericLedger extends BaseLedger<PolkadotGenericApp> {
     return this.withApp(async (app): Promise<LedgerSignature> => {
       const path = this.serializePath(accountOffset, addressOffset, accountOptions);
 
-      const rs = await this.wrapError(app.signRawEd25519(path, Buffer.from(wrapBytes(message))));
+      if (this.scheme === LEDGER_SCHEME.ECDSA) {
+        const { r, s, v } = await this.wrapError(app.signRawEcdsa(path, Buffer.from(message)));
 
-      const raw = hexStripPrefix(u8aToHex(rs.signature));
-      const firstByte = raw.slice(0, 2);
-      // Source: https://github.com/polkadot-js/common/blob/a82ebdf6f9d78791bd1f21cd3c534deee37e0840/packages/keyring/src/pair/index.ts#L29-L34
-      const isExtraByte = firstByte === '00';
-      // Remove first byte (signature_type) from signature
-      const signature = isExtraByte ? hexAddPrefix(raw.slice(2)) : hexAddPrefix(raw);
+        return {
+          signature: hexAddPrefix(u8aToHex(u8aConcat(r, s, v)))
+        };
+      } else {
+        const rs = await this.wrapError(app.signRawEd25519(path, Buffer.from(wrapBytes(message))));
 
-      return {
-        signature
-      };
+        const raw = hexStripPrefix(u8aToHex(rs.signature));
+        const firstByte = raw.slice(0, 2);
+        // Source: https://github.com/polkadot-js/common/blob/a82ebdf6f9d78791bd1f21cd3c534deee37e0840/packages/keyring/src/pair/index.ts#L29-L34
+        const isExtraByte = firstByte === '00';
+        // Remove first byte (signature_type) from signature
+        const signature = isExtraByte ? hexAddPrefix(raw.slice(2)) : hexAddPrefix(raw);
+
+        return {
+          signature
+        };
+      }
     });
   }
 
--- packages/extension-koni-ui/src/connector/Ledger/SubstrateMigrationLedger.ts
+++ packages/extension-koni-ui/src/connector/Ledger/SubstrateMigrationLedger.ts
@@ -1,16 +1,21 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { LEDGER_SCHEME } from '@subwallet/extension-base/background/KoniTypes';
 import { LedgerTypes } from '@subwallet/extension-koni-ui/types';
 
 import { SubstrateGenericLedger } from './SubstrateGenericLedger';
 
 export class SubstrateMigrationLedger extends SubstrateGenericLedger {
-  constructor (transport: LedgerTypes, slip44: number, ss58AddrType?: number) {
+  constructor (transport: LedgerTypes, slip44: number, ss58AddrType?: number, scheme?: LEDGER_SCHEME) {
     super(transport, slip44);
 
     if (ss58AddrType) {
       this.ss58_addr_type = ss58AddrType;
+    }
+
+    if (scheme) {
+      this.scheme = scheme;
     }
   }
 }
