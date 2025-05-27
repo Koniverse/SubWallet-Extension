4c1137f9bfcdbb83f3b5e3c73c144a85f5355788
tunghp2002 <atsimet@gmail.com>
2025-05-27 19:05:47 +0700

    [Backend-issue-38] Add simpleswap log

--- packages/extension-base/src/services/swap-service/handler/simpleswap-handler.ts
+++ packages/extension-base/src/services/swap-service/handler/simpleswap-handler.ts
@@ -106,6 +106,8 @@ const buildTxForSimpleSwap = async (params: BuildSimpleSwapTxParams): Promise<Bu
 
     const result = depositAddressResponse.result;
 
+    console.log('simpleswapID', result.id);
+
     if (!result?.id || !result.addressFrom || !result.amountTo) {
       return { error: new TransactionError(BasicTxErrorType.INTERNAL_ERROR) };
     }
