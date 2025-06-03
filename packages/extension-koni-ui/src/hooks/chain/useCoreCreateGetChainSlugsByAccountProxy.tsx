8b75bd1dc398d6b25fc0524e2dc4b29b9efb683c
lw <linhtm@konistudio.xyz>
2025-06-03 17:47:56 +0700

    [Issue-4297] refactor: Update logic for useCoreCreateGetChainSlugsByAccountProxy and useGetChainSlugsByCurrentAccountProxy

--- packages/extension-koni-ui/src/hooks/chain/useCoreCreateGetChainSlugsByAccountProxy.tsx
+++ packages/extension-koni-ui/src/hooks/chain/useCoreCreateGetChainSlugsByAccountProxy.tsx
@@ -1,13 +1,60 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { _ChainInfo, _ChainStatus } from '@subwallet/chain-list/types';
 import { AccountProxy } from '@subwallet/extension-base/types';
+import { isAccountAll } from '@subwallet/extension-base/utils';
+import { useSelector } from '@subwallet/extension-koni-ui/hooks';
+import { isChainInfoCompatibleWithAccountInfo } from '@subwallet/extension-koni-ui/utils';
 import { useCallback } from 'react';
 
+function getChainSlugsByAccountProxySingle (accountProxySingle: AccountProxy, chainInfoMap: Record<string, _ChainInfo>): string[] {
+  if (accountProxySingle.specialChain) {
+    return accountProxySingle.specialChain in chainInfoMap ? [accountProxySingle.specialChain] : [];
+  }
+
+  const result: string[] = [];
+
+  for (const chainInfo of Object.values(chainInfoMap)) {
+    if (accountProxySingle.accounts.some((account) => isChainInfoCompatibleWithAccountInfo(chainInfo, account.chainType, account.type))) {
+      result.push(chainInfo.slug);
+    }
+  }
+
+  return result;
+}
+
+function getChainSlugsByAccountProxyAll (accountProxyAll: AccountProxy, accountProxies: AccountProxy[], chainInfoMap: Record<string, _ChainInfo>): string[] {
+  const { specialChain } = accountProxyAll;
+
+  if (specialChain) {
+    return specialChain in chainInfoMap ? [specialChain] : [];
+  }
+
+  const slugSet = new Set<string>();
+
+  for (const accountProxy of accountProxies) {
+    for (const slug of getChainSlugsByAccountProxySingle(accountProxy, chainInfoMap)) {
+      slugSet.add(slug);
+    }
+  }
+
+  return [...slugSet];
+}
+
 const useCoreCreateGetChainSlugsByAccountProxy = () => {
-  return useCallback((accountProxies: AccountProxy): string[] => {
-    return [];
-  }, []);
+  const accountProxies = useSelector((state) => state.accountState.accountProxies);
+  const chainInfoMap = useSelector((state) => state.chainStore.chainInfoMap);
+
+  return useCallback((accountProxy: AccountProxy): string[] => {
+    const filteredChainInfoMap = Object.fromEntries(Object.entries(chainInfoMap).filter(([, chainInfo]) => chainInfo.chainStatus === _ChainStatus.ACTIVE));
+
+    if (isAccountAll(accountProxy.id)) {
+      return getChainSlugsByAccountProxyAll(accountProxy, accountProxies, filteredChainInfoMap);
+    }
+
+    return getChainSlugsByAccountProxySingle(accountProxy, filteredChainInfoMap);
+  }, [accountProxies, chainInfoMap]);
 };
 
 export default useCoreCreateGetChainSlugsByAccountProxy;
--- packages/extension-koni-ui/src/hooks/chain/useGetChainSlugsByCurrentAccountProxy.tsx
+++ packages/extension-koni-ui/src/hooks/chain/useGetChainSlugsByCurrentAccountProxy.tsx
@@ -1,8 +1,20 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { useCoreCreateGetChainSlugsByAccountProxy, useSelector } from '@subwallet/extension-koni-ui/hooks';
+import { useMemo } from 'react';
+
 const useGetChainSlugsByCurrentAccountProxy = (): string[] => {
-  return [];
+  const currentAccountProxy = useSelector((state) => state.accountState.currentAccountProxy);
+  const getChainSlugsByAccountProxy = useCoreCreateGetChainSlugsByAccountProxy();
+
+  return useMemo(() => {
+    if (!currentAccountProxy) {
+      return [];
+    }
+
+    return getChainSlugsByAccountProxy(currentAccountProxy);
+  }, [currentAccountProxy, getChainSlugsByAccountProxy]);
 };
 
 export default useGetChainSlugsByCurrentAccountProxy;
