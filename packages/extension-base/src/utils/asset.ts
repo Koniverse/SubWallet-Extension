4674e75382dad1ec4a4f6526f099d8be9e53a03e
Thiendekaco <139972251+Thiendekaco@users.noreply.github.com>
2025-07-01 09:44:09 +0700

    [Issue-4365] fix(ledger): refactor ledger substrate ecdsa multiAsset list

--- packages/extension-base/src/utils/asset.ts
+++ packages/extension-base/src/utils/asset.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { _AssetType, _ChainAsset, _ChainInfo } from '@subwallet/chain-list/types';
-import { IGNORE_SUBSTRATE_ECDSA_LEDGER_ASSETS_LIST } from '@subwallet/extension-base/constants';
+import { LEDGER_ECDSA_MULTI_ASSET_CHAINS } from '@subwallet/extension-base/constants';
 import { _getAssetNetuid, _getAssetOriginChain, _isNativeToken, _isSubstrateEvmCompatibleChain } from '@subwallet/extension-base/services/chain-service/utils';
 
 export const filterAssetsByChainAndType = (chainAssetMap: Record<string, _ChainAsset>, chain: string, assetTypes: _AssetType[]): Record<string, _ChainAsset> => {
@@ -34,9 +34,13 @@ export const filterAlphaAssetsByChain = (chainAssetMap: Record<string, _ChainAss
 };
 
 export const isSubstrateEcdsaLedgerAssetSupported = (chainsAsset: _ChainAsset, chainInfo: _ChainInfo) => {
-  if (IGNORE_SUBSTRATE_ECDSA_LEDGER_ASSETS_LIST.includes(chainInfo.slug)) {
+  if (!_isSubstrateEvmCompatibleChain(chainInfo)) {
+    return false;
+  }
+
+  if (!LEDGER_ECDSA_MULTI_ASSET_CHAINS.includes(chainInfo.slug)) {
     return _isNativeToken(chainsAsset);
   }
 
-  return _isSubstrateEvmCompatibleChain(chainInfo);
+  return _isNativeToken(chainsAsset) || !!chainsAsset.metadata?.assetId;
 };
