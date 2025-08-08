37854e5d7588f1757bf9fdbd575bf890002defd9
lw <linhtm@konistudio.xyz>
2025-08-08 14:53:25 +0700

    [Issue-4529] refactor: init OpenGov files

---
+++ packages/extension-koni-ui/src/components/Governance/VoteTypeLabel.tsx
@@ -0,0 +1,24 @@
+// Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { ThemeProps } from '@subwallet/extension-koni-ui/types';
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = ThemeProps;
+
+const Component = ({ className }: Props): React.ReactElement<Props> => {
+  return (
+    <div className={className}>
+
+    </div>
+  );
+};
+
+const VoteTypeLabel = styled(Component)<Props>(({ theme: { token } }: Props) => {
+  return {
+
+  };
+});
+
+export default VoteTypeLabel;
---
+++ packages/extension-koni-ui/src/components/Governance/index.tsx
@@ -0,0 +1,7 @@
+// Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+export { default as ReferendumItem } from './ReferendumItem';
+export { default as ReferendumTag } from './ReferendumTag';
+export { default as ReferendumVoteProgressBar } from './ReferendumVoteProgressBar';
+export { default as VoteTypeLabel } from './VoteTypeLabel';
