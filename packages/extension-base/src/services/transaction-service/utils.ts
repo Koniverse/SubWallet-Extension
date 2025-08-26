2f5537eb7e932ea219273a07d276474a6abbbae8
PDTnhah <phamduythanh43@gmail.com>
2025-08-26 17:26:15 +0700

    [Issue-4616] Update chainlist stable v0.2.113

--- packages/extension-base/src/services/transaction-service/utils.ts
+++ packages/extension-base/src/services/transaction-service/utils.ts
@@ -26,6 +26,10 @@ function getBlockExplorerAccountRoute (explorerLink: string) {
     return 'accounts';
   }
 
+  if (explorerLink.includes('explorer.comstats.org')) {
+    return 'accounts';
+  }
+
   if (explorerLink.includes('devnet-explorer.mosaicchain.io')) {
     return 'accounts';
   }
@@ -98,7 +102,7 @@ function getBlockExplorerTxRoute (chainInfo: _ChainInfo) {
     return '#/extrinsics';
   }
 
-  if (['edgeware'].includes(chainInfo.slug)) {
+  if (['edgeware', 'commune'].includes(chainInfo.slug)) {
     return 'extrinsics';
   }
 
