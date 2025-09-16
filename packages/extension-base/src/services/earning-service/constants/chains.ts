49c54b9d09491183fdb72c4041dd7a3e56daeced
bluezdot <72647326+bluezdot@users.noreply.github.com>
2025-09-16 17:32:55 +0700

    [Issue-4507] feat: add earning, balance & transfer

--- packages/extension-base/src/services/earning-service/constants/chains.ts
+++ packages/extension-base/src/services/earning-service/constants/chains.ts
@@ -3,7 +3,7 @@
 
 export const _STAKING_CHAIN_GROUP = {
   relay: ['polkadot', 'kusama', 'aleph', 'polkadex', 'ternoa', 'alephTest', 'polkadexTest', 'westend', 'kate', 'edgeware', 'creditcoin', 'vara_network', 'goldberg_testnet', 'availTuringTest', 'avail_mainnet', 'vara_testnet', 'dentnet', 'cere', 'statemine', 'statemint', 'westend_assethub', 'paseo_assethub'],
-  assetHub: ['statemine', 'statemint', 'westend_assethub', 'paseo_assethub'],
+  assetHub: ['statemine', 'statemint'], // chain in these group is ignore displaying staking.
   para: ['moonbeam', 'moonriver', 'moonbase', 'turing', 'turingStaging', 'bifrost', 'bifrost_testnet', 'calamari_test', 'calamari', 'manta_network', 'polimec'],
   astar: ['astar', 'shiden', 'shibuya'],
   amplitude: ['amplitude', 'amplitude_test', 'pendulum', 'krest_network'], // amplitude and kilt only share some common logic
