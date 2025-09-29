029961269c5b81cd6f7c525addd44b5f35331aa9
frenkie <dungtpa2@gmail.com>
2025-09-29 10:34:27 +0700

    [ISSUE-4555] fix(gov-self-test): make only the loading button clickable (disable other buttons)

--- packages/extension-koni-ui/src/Popup/Transaction/variants/Governance/ReferendumVote/ReferendumStandardVote.tsx
+++ packages/extension-koni-ui/src/Popup/Transaction/variants/Governance/ReferendumVote/ReferendumStandardVote.tsx
@@ -316,6 +316,13 @@ const Component = (props: ComponentProps): React.ReactElement<ComponentProps> =>
     return voteMap.get(fromValue.toLowerCase());
   }, [voteMap, fromValue]);
 
+  const getButtonProps = (type: GovVoteType) => {
+    return {
+      loading: loadingButton === type,
+      disabled: (loadingButton !== null && loadingButton !== type) || (isDisable || !isBalanceReady)
+    };
+  };
+
   const previousVoteAmountDetail = useMemo<PreviousVoteAmountDetail | undefined>(() => getPreviousVoteAmountDetail(voteInfo), [voteInfo]);
 
   useEffect(() => {
@@ -506,24 +513,22 @@ const Component = (props: ComponentProps): React.ReactElement<ComponentProps> =>
       <TransactionFooter className={`${className} -transaction-footer`}>
         <div className={'__vote-buttons-container'}>
           <VoteButton
-            disabled={isDisable || !isBalanceReady}
-            loading={loadingButton === GovVoteType.NAY}
+            {...getButtonProps(GovVoteType.NAY)}
             onClick={onPreCheck(handleClickNay, ExtrinsicType.GOV_VOTE)}
             type={GovVoteType.NAY}
           />
           <VoteButton
-            loading={loadingButton === GovVoteType.ABSTAIN}
+            {...getButtonProps(GovVoteType.ABSTAIN)}
             onClick={goRefAbstainVote}
             type={GovVoteType.ABSTAIN}
           />
           <VoteButton
-            loading={loadingButton === GovVoteType.SPLIT}
+            {...getButtonProps(GovVoteType.SPLIT)}
             onClick={goRefSplitVote}
             type={GovVoteType.SPLIT}
           />
           <VoteButton
-            disabled={isDisable || !isBalanceReady}
-            loading={loadingButton === GovVoteType.AYE}
+            {...getButtonProps(GovVoteType.AYE)}
             onClick={onPreCheck(handleClickAye, ExtrinsicType.GOV_VOTE)}
             type={GovVoteType.AYE}
           />
