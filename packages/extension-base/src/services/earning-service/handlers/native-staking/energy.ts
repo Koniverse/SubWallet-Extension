21e28a0a89f0735ca4dec2624ca80be9a76e6de9
tunghp2002 <atsimet@gmail.com>
2025-09-29 17:12:58 +0700

    [Issue-4694] Remove log

--- packages/extension-base/src/services/earning-service/handlers/native-staking/energy.ts
+++ packages/extension-base/src/services/earning-service/handlers/native-staking/energy.ts
@@ -422,8 +422,6 @@ export default class EnergyNativeStakingPoolHandler extends BaseParaNativeStakin
     const { address: selectedCollatorAddress } = selectedCollatorInfo;
     const onchainSelectedCollatorNominatorCount = onchainCollatorInfo.nominationCount;
 
-    console.log('checking', onchainCollatorInfo);
-
     const compoundResult = (
       extrinsic: SubmittableExtrinsic<'promise'>
     ): Promise<[TransactionData, YieldTokenBaseInfo]> => {
