d57a19bf2ac1de88205e4bc1344e9836a25babe6
Quangdm-cdm <85857151+Quangdm-cdm@users.noreply.github.com>
2025-09-30 15:39:29 +0700

    [Issue-1988-mobile] Fix bug cannot withdraw on Westend Asset Hub

--- packages/extension-base/src/services/earning-service/handlers/native-staking/relay-chain.ts
+++ packages/extension-base/src/services/earning-service/handlers/native-staking/relay-chain.ts
@@ -181,7 +181,7 @@ export default class RelayNativeStakingPoolHandler extends BaseNativeStakingPool
 
   /* Subscribe pool position */
 
-  async parseNominatorMetadata (chainInfo: _ChainInfo, address: string, substrateApi: _SubstrateApi, ledger: PalletStakingStakingLedger, currentEra: string, minStake: BN, _deriveSessionProgress: DeriveSessionProgress): Promise<Omit<YieldPositionInfo, keyof BaseYieldPositionInfo>> {
+  async parseNominatorMetadata (chainInfo: _ChainInfo, address: string, substrateApi: _SubstrateApi, ledger: PalletStakingStakingLedger, currentEra: string, minStake: BN): Promise<Omit<YieldPositionInfo, keyof BaseYieldPositionInfo>> {
     const chain = chainInfo.slug;
 
     const [_nominations, _bonded, _activeEra] = await Promise.all([
@@ -333,7 +333,7 @@ export default class RelayNativeStakingPoolHandler extends BaseNativeStakingPool
       }
 
       if (ledgers) {
-        const [_currentEra, _minimumActiveStake, _minNominatorBond, _deriveSessionProgress] = await Promise.all([
+        const [_currentEra, _minimumActiveStake, _minNominatorBond] = await Promise.all([
           substrateApi.api.query?.staking?.currentEra(),
           substrateApi.api.query?.staking?.minimumActiveStake && substrateApi.api.query?.staking?.minimumActiveStake(),
           substrateApi.api.query?.staking?.minNominatorBond(),
@@ -352,7 +352,7 @@ export default class RelayNativeStakingPoolHandler extends BaseNativeStakingPool
           const ledger = _ledger.toPrimitive() as unknown as PalletStakingStakingLedger;
 
           if (ledger) {
-            const nominatorMetadata = await this.parseNominatorMetadata(chainInfo, owner, substrateApi, ledger, currentEra, minStake, _deriveSessionProgress);
+            const nominatorMetadata = await this.parseNominatorMetadata(chainInfo, owner, substrateApi, ledger, currentEra, minStake);
 
             resultCallback({
               ...defaultInfo,
