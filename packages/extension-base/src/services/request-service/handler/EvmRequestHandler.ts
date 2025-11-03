865dd9e5412088defe7425ca33363a62ecd41718
bluezdot <72647326+bluezdot@users.noreply.github.com>
2025-11-03 12:05:15 +0700

    [Issue-3632] fix: improve round evm value

--- packages/extension-base/src/services/request-service/handler/EvmRequestHandler.ts
+++ packages/extension-base/src/services/request-service/handler/EvmRequestHandler.ts
@@ -217,12 +217,12 @@ export default class EvmRequestHandler {
     const pair = keyring.getPair(from as string);
     const params = {
       ...transaction,
-      gas: anyNumberToBN(gas).toFixed(),
+      gas: anyNumberToBN(gas).toNumber(),
       value: anyNumberToBN(value).toFixed(),
-      gasPrice: anyNumberToBN(gasPrice).toFixed(),
-      gasLimit: anyNumberToBN(estimateGas).toFixed(),
-      maxFeePerGas: anyNumberToBN(maxFeePerGas).toFixed(),
-      maxPriorityFeePerGas: anyNumberToBN(maxPriorityFeePerGas).toFixed()
+      gasPrice: anyNumberToBN(gasPrice).toNumber(),
+      gasLimit: anyNumberToBN(estimateGas).toNumber(),
+      maxFeePerGas: anyNumberToBN(maxFeePerGas).toNumber(),
+      maxPriorityFeePerGas: anyNumberToBN(maxPriorityFeePerGas).toNumber()
       // nonce: await web3.eth.getTransactionCount(from) // Todo: fill this value from transaction service
     } as TransactionConfig;
 
