2cb29bde58551f284df451ddafa44594285b8830
PDTnhah <phamduythanh43@gmail.com>
2025-11-25 09:57:46 +0700

    [Issue-4676] fix lint

--- packages/extension-base/src/utils/fee/transfer.ts
+++ packages/extension-base/src/utils/fee/transfer.ts
@@ -18,23 +18,7 @@ import { _isPolygonChainBridge } from '@subwallet/extension-base/services/balanc
 import { _isPosChainBridge } from '@subwallet/extension-base/services/balance-service/transfer/xcm/posBridge';
 import { estimateXcmFee } from '@subwallet/extension-base/services/balance-service/transfer/xcm/utils';
 import { _BitcoinApi, _CardanoApi, _EvmApi, _SubstrateApi, _TonApi } from '@subwallet/extension-base/services/chain-service/types';
-import {
-  _getAssetDecimals,
-  _getAssetExistentialDeposit,
-  _getContractAddressOfToken,
-  _isChainBitcoinCompatible,
-  _isChainCardanoCompatible,
-  _isChainEvmCompatible,
-  _isChainTonCompatible,
-  _isLocalToken,
-  _isNativeToken,
-  _isPureEvmChain,
-  _isTokenEvmSmartContract,
-  _isTokenTransferredByBitcoin,
-  _isTokenTransferredByCardano,
-  _isTokenTransferredByEvm,
-  _isTokenTransferredByTon
-} from '@subwallet/extension-base/services/chain-service/utils';
+import { _getAssetDecimals, _getAssetExistentialDeposit, _getContractAddressOfToken, _isChainBitcoinCompatible, _isChainCardanoCompatible, _isChainEvmCompatible, _isChainTonCompatible, _isLocalToken, _isNativeToken, _isPureEvmChain, _isTokenEvmSmartContract, _isTokenTransferredByBitcoin, _isTokenTransferredByCardano, _isTokenTransferredByEvm, _isTokenTransferredByTon } from '@subwallet/extension-base/services/chain-service/utils';
 import { calculateToAmountByReservePool, FEE_COVERAGE_PERCENTAGE_SPECIAL_CASE } from '@subwallet/extension-base/services/fee-service/utils';
 import { getHydrationRate } from '@subwallet/extension-base/services/fee-service/utils/tokenPayFee';
 import { isCardanoTransaction, isTonTransaction } from '@subwallet/extension-base/services/transaction-service/helpers';
