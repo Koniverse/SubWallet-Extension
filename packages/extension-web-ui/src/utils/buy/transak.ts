b3061378598744915fc2f9e73bdbe8c11eaf1f28
tunghp2002 <atsimet@gmail.com>
2025-11-25 14:20:02 +0700

    [Issue-4835-webapp] Update Transak Widget URL by using sdk

--- packages/extension-web-ui/src/utils/buy/transak.ts
+++ packages/extension-web-ui/src/utils/buy/transak.ts
@@ -1,33 +1,23 @@
 // Copyright 2019-2022 @subwallet/extension-web-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { TRANSAK_API_KEY, TRANSAK_URL } from '@subwallet/extension-web-ui/constants';
 import { CreateBuyOrderFunction } from '@subwallet/extension-web-ui/types';
-import qs from 'querystring';
+import subwalletApiSdk from '@subwallet-monorepos/subwallet-services-sdk';
 
 export const createTransakOrder: CreateBuyOrderFunction = (orderParams) => {
   const { action = 'BUY', address, network, slug = '', symbol } = orderParams;
 
-  return new Promise((resolve) => {
-    const location = window.location.origin;
-    const params: Record<string, string | number | boolean | null> = {
-      apiKey: TRANSAK_API_KEY,
-      defaultCryptoCurrency: symbol,
-      networks: network,
-      cryptoCurrencyList: symbol,
-      productsAvailed: action || 'BUY'
-    };
+  const location = window.location.origin;
+  // partnerCustomerId is used to determine which account will sell tokens
+  const redirectURL = `${location}/off-ramp-loading?slug=${slug ?? ''}&partnerCustomerId=${address}`;
 
-    if (action === 'BUY') {
-      params.walletAddress = address;
-    } else {
-      params.partnerCustomerId = address;
-      params.redirectURL = `${location}/off-ramp-loading?slug=${slug ?? ''}`;
-      params.walletRedirection = true;
-    }
-
-    const query = qs.stringify(params);
-
-    resolve(`${TRANSAK_URL}?${query}`);
+  return subwalletApiSdk.transakApi.generateOrderUrl({
+    symbol: symbol,
+    address: address,
+    network: network,
+    walletReference: address,
+    action: action,
+    redirectURL: redirectURL,
+    referrerDomain: 'https://web.subwallet.app'
   });
 };
