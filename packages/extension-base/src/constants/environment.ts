5678c2a8d7db884cc739ab63324ce921c367e2ea
tunghp2002 <atsimet@gmail.com>
2025-12-02 16:25:26 +0700

    [Issue-#4835] Webapp: revert env

--- packages/extension-base/src/constants/environment.ts
+++ packages/extension-base/src/constants/environment.ts
@@ -6,7 +6,7 @@ const branchName = process.env.BRANCH_NAME || 'subwallet-dev';
 
 export const APP_VERSION = process.env.PKG_VERSION || '';
 export const isProductionMode = PRODUCTION_BRANCHES.indexOf(branchName) > -1;
-export const BACKEND_API_URL = process.env.SUBWALLET_API || (isProductionMode ? 'https://sw-services.subwallet.app/api' : 'https://be-dev-02.subwallet.app/api');
+export const BACKEND_API_URL = process.env.SUBWALLET_API || (isProductionMode ? 'https://sw-services.subwallet.app/api' : 'https://be-dev.subwallet.app/api');
 export const BACKEND_PRICE_HISTORY_URL = process.env.SUBWALLET_PRICE_HISTORY_API || (isProductionMode ? 'https://price-history.subwallet.app/api' : 'https://price-history-dev.subwallet.app/api');
 
 export const SW_EXTERNAL_SERVICES_API = process.env.SW_EXTERNAL_SERVICES_API || (isProductionMode ? 'https://external-services.subwallet.app' : 'https://external-services-dev.subwallet.app');
