94fd1551888adafb08eeba9bea17b844179793fc
bluezdot <72647326+bluezdot@users.noreply.github.com>
2025-12-08 16:54:58 +0700

    [Issue-4870] chores: add todos

--- packages/extension-base/src/services/keyring-service/context/handlers/Multisig.ts
+++ packages/extension-base/src/services/keyring-service/context/handlers/Multisig.ts
@@ -3,6 +3,7 @@
 
 import { RequestAccountCreateMultisig } from '@subwallet/extension-base/background/KoniTypes';
 import { AccountBaseHandler } from '@subwallet/extension-base/services/keyring-service/context/handlers/Base';
+import { keyring } from '@subwallet/ui-keyring';
 
 /**
  * @class AccountMultisigHandler
@@ -18,10 +19,17 @@ export class AccountMultisigHandler extends AccountBaseHandler {
     console.log('threshold', threshold);
 
     // todo: a function to generate multisig address
+    const multisigAddress = '1627ti7gKnn5aTp7a7SUVsgnM9wE6BCNw6CgCzKiVeJz5DDA';
     // todo: check exist address
     // todo: check exist account name
 
     // todo: add address, metadata. keyring.keyring.addFromAddress(address, meta, null, type)?
+    const rs = keyring.keyring.addFromAddress(multisigAddress, { name, threshold, signers });
+
+    console.log('Multisig account added to keyring:', rs);
+
+    // todo: Mục đích của saveAccount là gì? Có cần thiết không?
+    // keyring.saveAccount(rs)
     // todo: update state modifyPair. this.state.upsertModifyPairs(modifiedPairs)?
     // todo: saveCurrentAccountProxyId?
 
