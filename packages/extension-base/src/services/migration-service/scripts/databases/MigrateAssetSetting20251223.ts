538d6dd2a2e27c43ddad44f3bda8fd8249d6dc6f
PDTnhah <phamduythanh43@gmail.com>
2025-12-23 11:37:13 +0700

    [Issue-4861] feat: update chainlist - update symbol

---
+++ packages/extension-base/src/services/migration-service/scripts/databases/MigrateAssetSetting20251223.ts
@@ -0,0 +1,35 @@
+// Copyright 2019-2022 @subwallet/extension-koni authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { AssetSetting } from '@subwallet/extension-base/background/KoniTypes';
+import BaseMigrationJob from '@subwallet/extension-base/services/migration-service/Base';
+
+export default class MigrateAssetSetting20251107 extends BaseMigrationJob {
+  public override async run (): Promise<void> {
+    try {
+      const changeSlugsMap: Record<string, string> = {
+        'gnosis-NATIVE-xDAI': 'gnosis-NATIVE-XDAI',
+        'stable-ERC20-USD₮0-0x779Ded0c9e1022225f8E0630b35a9b54bE713736': 'stable-ERC20-USDT0-0x779Ded0c9e1022225f8E0630b35a9b54bE713736'
+      };
+
+      const assetSetting = await this.state.chainService.getAssetSettings();
+
+      const migratedAssetSetting: Record<string, AssetSetting> = {};
+
+      for (const [oldSlug, newSlug] of Object.entries(changeSlugsMap)) {
+        if (Object.keys(assetSetting).includes(oldSlug)) {
+          const isVisible = assetSetting[oldSlug].visible;
+
+          migratedAssetSetting[newSlug] = { visible: isVisible };
+        }
+      }
+
+      this.state.chainService.setAssetSettings({
+        ...assetSetting,
+        ...migratedAssetSetting
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
