d07dc926196c6182ee87ab9861239e286fa5610b
frenkie <dungtpa2@gmail.com>
2026-01-27 16:01:36 +0700

    [Issue-#4884] fix eslint

--- packages/extension-koni-ui/src/hooks/nft/useGetParent.ts
+++ packages/extension-koni-ui/src/hooks/nft/useGetParent.ts
@@ -1,20 +1,25 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {useMemo} from 'react';
-import {NftItem} from '@subwallet/extension-base/background/KoniTypes';
-import {useGetNftByAccount} from '@subwallet/extension-koni-ui/hooks';
+import { NftItem } from '@subwallet/extension-base/background/KoniTypes';
+import { useGetNftByAccount } from '@subwallet/extension-koni-ui/hooks';
+import { useMemo } from 'react';
 
 const findNftDeep = (items: NftItem[], targetId: string, chain: string, collectionId: string): NftItem | undefined => {
   for (const item of items) {
     if (item.id === targetId && item.chain === chain && item.collectionId === collectionId) {
       return item;
     }
+
     if (item.nestingTokens && item.nestingTokens.length > 0) {
       const foundChild = findNftDeep(item.nestingTokens, targetId, chain, collectionId);
-      if (foundChild) return foundChild;
+
+      if (foundChild) {
+        return foundChild;
+      }
     }
   }
+
   return undefined;
 };
 
