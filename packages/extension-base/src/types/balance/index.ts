3e9c124a8e29c1efcd2c7ca7973e114283dc87bb
tunghp2002 <atsimet@gmail.com>
2026-05-19 18:52:37 +0700

    [Issue-#4946] Chore: fix lint

--- packages/extension-base/src/types/balance/index.ts
+++ packages/extension-base/src/types/balance/index.ts
@@ -18,7 +18,7 @@ export enum BalanceType {
   TOTAL = 'total', // free + locked
   TOTAL_MINUS_RESERVED = 'totalMinusReserved', // free + locked - reserved
   KEEP_ALIVE = 'keepAlive',
-  STAKING = 'staking' // staking balance,
+  STAKING = 'staking', // staking balance
   TOTAL_STAKE_EQUIVALENT = 'totalStakeEquivalent', // only for bittensor (delegate staking)
   TOTAL_EQUIVALENT = 'totalEquivalent' // only for bittensor (delegate staking)
 }
